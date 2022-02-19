e3f35f3c05ab71e3e86a8ebb3dc7e0664a8edec7
Mateus Souza <mateus-souza2001@uol.com.br>
2022-02-19 12:28:41 -0300

    retomar da secao 1.2, falando de tipos

---
+++ index.js
@@ -0,0 +1,30 @@
+var idade = 18
+console.log("Oi " + nome)
+if (idade >= 18){
+    let nome = "João"
+    console.log("Parabens, " + nome)
+    console.log("Você pode dirigir")
+}
+console.log("Até mais " + nome)
+
+// var linguagem = "JavaScript"
+// console.log("Aprendendo " + linguagem)
+// //Variavel pode ser redeclarada
+// var linguagem = "java"
+// console.log("Aprendendo " + linguagem)
+
+// const nome = "José"
+// const endereco = `Rua K, 12`
+// const sexo = "F"
+// const idade = 27
+
+
+// let a = 2
+// a = 3
+// let nomeCompleto = "Joao Silva"
+// a = "abc"
+
+
+// var c = 2 + 3
+// c = 5
+// var d = " abcd"
